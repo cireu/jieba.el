7c34ac85e325d331c57e581d966ed8b66c8c73e2
Zhu Zihao <all_but_last@163.com>
2019-05-23 23:34:00 +0800

    Refactor on nodejieba wrapper.

--- stdio-jrpc.js
+++ stdio-jrpc.js
@@ -36,6 +36,26 @@ const RPCErrors = {
 
 class RPCConnection {
 
+  constructor () {
+    this._methodTable = new Map()
+    this._currentHandlingId = null
+
+    process.stdin.setEncoding('utf-8')
+    process.stdin.on('readable', () => {
+      let data = ''
+      let chunk
+      while (true) {
+        chunk = process.stdin.read()
+        if (chunk === null) {
+          break
+        } else {
+          data += chunk
+        }
+      }
+      this._receiveRaw(data)
+    })
+  }
+
   _sendRaw (rawMsg) {
     process.stdout.write(rawMsg)
   }
@@ -75,26 +95,6 @@ class RPCConnection {
 
   handle (methodName, callback) {
     this._methodTable.set(methodName, callback)
-  }
-
-  constructor () {
-    this._methodTable = new Map()
-    this._currentHandlingId = null
-
-    process.stdin.setEncoding('utf-8')
-    process.stdin.on('readable', () => {
-      let data = ''
-      let chunk
-      while (true) {
-        chunk = process.stdin.read()
-        if (chunk === null) {
-          break
-        } else {
-          data += chunk
-        }
-      }
-      this._receiveRaw(data)
-    })
   }
 
   static open () {
